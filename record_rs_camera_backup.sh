8a85b4575e1753ace7b2e265e27b97a99e75a5fa
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-19 11:43:55 +0900

    [Perf]: Drop the raw depth image for data recording
    It is too computationally expensive to all record the image data.
    Depth aligned images remain.

--- record_rs_camera_backup.sh
+++ record_rs_camera_backup.sh
@@ -6,8 +6,9 @@ mkdir -p ~/Downloads/$2
 rosbag record --lz4 -o ~/Downloads/$2/$1.bag /camera/color/metadata /camera/depth/metadata \
   /tf_static \
   /camera/color/camera_info \
-  /camera/aligned_depth_to_color/camera_info \
+  /camera/depth/camera_info \
   /camera/color/image_raw/compressed \
+  /camera/depth/image_rect_raw/compressedDepth \
   /camera/aligned_depth_to_color/image_raw/compressedDepth \
   /camera/extrinsics/depth_to_color \
   /camera/accel/imu_info \
