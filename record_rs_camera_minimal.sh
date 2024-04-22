7a28636ef6577c915bd87fd3e242999268901716
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-22 16:55:48 +0900

    [Perf]: Revert to save all data

--- record_rs_camera_minimal.sh
+++ record_rs_camera_minimal.sh
@@ -6,15 +6,7 @@ mkdir -p ~/Downloads/$2
 rosbag record --lz4 -O ~/Downloads/$2/$1 /camera/color/metadata /camera/depth/metadata \
   /tf_static \
   /camera/color/camera_info \
-  /camera/depth/camera_info \
   /camera/aligned_depth_to_color/camera_info \
   /camera/color/image_raw/compressed \
-  /camera/depth/image_rect_raw/compressedDepth \
   /camera/aligned_depth_to_color/image_raw/compressedDepth \
-  /camera/extrinsics/depth_to_color \
-  /camera/accel/imu_info \
-  /camera/accel/metadata \
-  /camera/accel/sample \
-  /camera/gyro/imu_info \
-  /camera/gyro/metadata \
-  /camera/gyro/sample
+  /camera/extrinsics/depth_to_color
