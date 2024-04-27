3b429fb629bd0c51833f5fd1292d9b67c1624480
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-04-27 14:47:03 +0900

    [Feat]: Support multiple cameras in the launch file

--- record_rs_camera_imu.sh
+++ record_rs_camera_imu.sh
@@ -10,4 +10,10 @@ rosbag record -O ~/Downloads/$2/$3 \
   /$1/aligned_depth_to_color/camera_info \
   /$1/color/image_raw/compressed \
   /$1/depth/image_rect_raw/compressedDepth \
-  /$1/aligned_depth_to_color/image_raw/compressedDepth
+  /$1/aligned_depth_to_color/image_raw/compressedDepth \
+  /$1/accel/imu_info \
+  /$1/accel/metadata \
+  /$1/accel/sample \
+  /$1/gyro/imu_info \
+  /$1/gyro/metadata \
+  /$1/gyro/sample
