de3f5f54e9425f2a9145c3041b259b0ef430d0e1
Ikhyeon Cho <tre0430@korea.ac.kr>
2024-05-23 13:34:41 +0900

    [Feat]: Rename the topics with consistency to other tools package

--- record_rs_camera.sh
+++ record_rs_camera.sh
@@ -6,8 +6,8 @@ mkdir -p ~/Downloads/$2
 rosbag record -O ~/Downloads/$2/$3 \
   /tf_static \
   /$1/color/camera_info \
+  /$1/color/image/compressed \
   /$1/depth/camera_info \
-  /$1/aligned_depth_to_color/camera_info \
-  /$1/color/image_raw/compressed \
-  /$1/depth/image_rect_raw/compressedDepth \
-  /$1/aligned_depth_to_color/image_raw/compressedDepth
+  /$1/depth/image/compressedDepth \
+  /$1/depth_aligned/camera_info \
+  /$1/depth_aligned/image/compressedDepth
--- record_rs_camera_imu.sh
+++ record_rs_camera_imu.sh
@@ -6,11 +6,11 @@ mkdir -p ~/Downloads/$2
 rosbag record -O ~/Downloads/$2/$3 \
   /tf_static \
   /$1/color/camera_info \
+  /$1/color/image/compressed \
   /$1/depth/camera_info \
-  /$1/aligned_depth_to_color/camera_info \
-  /$1/color/image_raw/compressed \
-  /$1/depth/image_rect_raw/compressedDepth \
-  /$1/aligned_depth_to_color/image_raw/compressedDepth \
+  /$1/depth/image/compressedDepth \
+  /$1/depth_aligned/camera_info \
+  /$1/depth_aligned/image/compressedDepth
   /$1/accel/imu_info \
   /$1/accel/metadata \
   /$1/accel/sample \
